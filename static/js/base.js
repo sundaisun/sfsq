20cb4bc2ee76e44b00041ac5e3e244d0d456216e
Sun Dai <sundaisun@fb.com>
2018-06-26 20:11:00 -0700

    initial commit

---
+++ static/js/base.js
@@ -0,0 +1,81 @@
+//SHOW WHEN SCROLL UP
+ var previousScroll = 0;
+
+  $(window).scroll(function(){
+
+    var currentScroll = $(this).scrollTop();
+
+    /*
+      If the current scroll position is greater than 0 (the top) AND the current scroll position is less than the document height minus the window height (the bottom) run the navigation if/else statement.
+    */
+    if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
+      /*
+        If the current scroll is greater than the previous scroll (i.e we're scrolling down the page), hide the nav.
+      */
+      if (currentScroll > previousScroll){
+        window.setTimeout(hideNav, 300);
+      /*
+        Else we are scrolling up (i.e the previous scroll is greater than the current scroll), so show the nav.
+      */
+      } else {
+        window.setTimeout(showNav, 300);
+      }
+      /* 
+        Set the previous scroll value equal to the current scroll.
+      */
+      previousScroll = currentScroll;
+    }
+
+  });
+
+  function hideNav() {
+    $("[data-nav-status='toggle']").removeClass("is-visible").addClass("is-hidden");
+  }
+  function showNav() {
+    $("[data-nav-status='toggle']").removeClass("is-hidden").addClass("is-visible");
+  }
+
+
+
+
+//MENU TOGGLE ANIMATION
+    var theToggle = document.getElementById('toggle');
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+//   return false;
+}
+
+
